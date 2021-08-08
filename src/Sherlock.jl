d08d79087519e89de58686dad77d4d7cb8009749
Azzaare <jf@baffier.fr>
2021-08-08 13:28:58 +0900

    Reshape root file

--- src/Sherlock.jl
+++ src/Sherlock.jl
@@ -1,17 +1,44 @@
 module Sherlock
-    using Plots, LightGraphs, GraphRecipes, InteractiveUtils, Blink, Interact
-    greet() = print("My mind rebels at stagnation, give me problems, give me work!")
 
-    include("Types.jl")
-    export ENTITY_TYPES, Detective, functions, types, abstracttypes,
-        undefined, inquire, safeisfield, safeisabstract,
-        safeisnotabstract, ismacro
+# Usings and imports
+using Blink
+using GraphRecipes
+using Interact
+using InteractiveUtils
+using LightGraphs
+using Plots
 
-    include("Utilities.jl")
-    export nothing_is_val, trim_type, typetype_edges, functiontype_edges
-            #pkgchildren, loaded_packages #irreprable error with these...
+# Exports: types.jl
+export abstracttypes
+export Detective
+export ENTITY_TYPES
+export functions
+export inquire
+export ismacro
+export safeisabstract
+export safeisfield
+export safeisnotabstract
+export types
+export undefined
 
-    include("Visualizations.jl")
-    export magnify, MIMO, typetree, sherlockplot, sherlock_UI
+# Exports: utilities.jl
+export functiontype_edges
+export greet
+export nothing_is_val
+export trim_type
+export typetype_edges
+#pkgchildren, loaded_packages #irreprable error with these...
+
+# Exports: visualizations.jl
+export magnify
+export MIMO
+export sherlockplot
+export sherlock_UI
+export typetree
+
+# Includes
+include("types.jl")
+include("utilities.jl")
+include("visualizations.jl")
 
 end # module
